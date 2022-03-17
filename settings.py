f408094dc179f7acf3a83bab32bfc2f3b09166d8
rajeshai <73292655+rajeshai@users.noreply.github.com>
2022-03-17 13:41:10 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,1 +1,4 @@
-ALLOWED_HOSTS = ['mercury-ml-web-app.herokuapp.com']
+ALLOWED_HOSTS = ['*','mercury-ml-web-app.herokuapp.com','https://mercury-ml-web-app.herokuapp.com/', 'http://mercury-ml-web-app.herokuapp.com/',
+                '127.0.0.1',
+                'https://mercury-ml-web-app.herokuapp.com',
+                'http://mercury-ml-web-app.herokuapp.com']
