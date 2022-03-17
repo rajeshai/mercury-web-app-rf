00c8115181d1895e48a9e738e86a45679807145c
rajeshai <73292655+rajeshai@users.noreply.github.com>
2022-03-17 15:47:07 +0530

    Delete settings.py

--- settings.py
+++
@@ -1,6 +0,0 @@
-import django_heroku
-ALLOWED_HOSTS = ['*','mercury-ml-web-app.herokuapp.com','https://mercury-ml-web-app.herokuapp.com/', 'http://mercury-ml-web-app.herokuapp.com/',
-                '127.0.0.1',
-                'https://mercury-ml-web-app.herokuapp.com',
-                'http://mercury-ml-web-app.herokuapp.com']
-django_heroku.settings(locals())
