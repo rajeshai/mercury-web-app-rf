281dfad614543fa7069b3de3b148287ac8350049
rajeshai <73292655+rajeshai@users.noreply.github.com>
2022-03-17 15:03:48 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
+import django_heroku
 ALLOWED_HOSTS = ['*','mercury-ml-web-app.herokuapp.com','https://mercury-ml-web-app.herokuapp.com/', 'http://mercury-ml-web-app.herokuapp.com/',
                 '127.0.0.1',
                 'https://mercury-ml-web-app.herokuapp.com',
                 'http://mercury-ml-web-app.herokuapp.com']
+django_heroku.settings(locals())
